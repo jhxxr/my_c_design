9dd6ca124e0b79c57894a3e277060488081f855f
jhxxr <2471717907@qq.com>
2022-06-20 07:11:35 +0000

    6/200

--- ces.c
+++ ces.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main (int argc,char *argv[]){
     int i;
 	char align= '1';
 	int width = 10;
+    int start = 1;
+    char message[100]={0};
     for (i = 0;i<argc;i++){
         if (argv[i][0]=='-'){
             switch(argv[i][0]){
@@ -13,12 +15,21 @@ int main (int argc,char *argv[]){
                 case 'r':
                 case 'c':
                    align = argv[i][0];
+                   if (i+1>start)
+                       start = i+1;
                    break;
                 case 'w':
                    width = atoi(argv[i+1]);
+                   if (i+2>start)
+                       start = i+2;
                    break;
             }
         }  
     }
-    printf ("slign=%c,width = %d",align,width);
+    for (i=start ;i<argc-1;i++){
+        strcat(message,argv[i]);
+        strcat(message,' ');
+    }
+    strcat(message,argv[i]);
+    printf ("slign=%c,width=%d,message=%c",align,width,message);
 }
