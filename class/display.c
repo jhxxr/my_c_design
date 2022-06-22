ef8365fe3218dcabb8bc0bb34eac48e5d61a657b
jhxxr <2471717907@qq.com>
2022-06-22 01:22:53 +0000

    ???

---
+++ class/display.c
@@ -0,0 +1,55 @@
+#include "loadfonts.h"
+void displayline(char *start,char *end ,char align ,int width){
+    char buffer[8][200]={0};
+    int i;
+    char *p;
+    for(i=0;i<8;i++){
+        for (p=start;p<end;p++){
+            strcat(buffer[i],getfont(*p).img[i])
+        }
+    }
+    if (align=='c'){
+        sp=(width-strlen(buffer[0]))/2;
+    }else if (align =='r'){
+        sp =width-strlen(buffer[0]);
+
+    }
+    for(i=0;i<8;i++){
+        for(j=0;j<sp;j++){
+            printf(" ")
+
+        }
+        printf("%s\n",buffer[i]);
+    }
+}
+
+char *split(char *start,int width);
+void display(char align,int width,char message[]){
+    char *s=message;
+    har *e;
+    while(s!='\0'){
+        e=split(*s,width);
+        displayline(s,e,align,width);
+        s=e+1;
+
+    }
+}
+
+
+char * split(char *start,int width){
+    char *p=start;
+    int cur =0;
+    struct font f;
+    while(*p='\0'){
+        f= getfont(*p);
+        if (f.width+cur=<width){
+            cur +=f.width;
+            p++;
+        }else {
+            break;
+
+        }
+    }
+    return --p;
+}
+
